49533c0be925b12950c7d4fd0285592a99b53a77
Witold Adamus <witold.adamus@xsolve.pl>
2017-06-05 23:18:54 +0200

    Remove redundant code

--- src/components/LocationListing.jsx
+++ src/components/LocationListing.jsx
@@ -1,31 +1,43 @@
 import React, { Component } from 'react';
-import LocationList from './LocationList/LocationList'
-import LocationListActionKeep from './LocationList/Actions/LocationListActionKeep'
-import LocationListActionForger from './LocationList/Actions/LocationListActionForget'
+
+class LocationRow extends Component {
+  render = () => (
+      <ul>
+        {this.props.items.map(location =>
+          <li key={location.woeid}>
+            {location.title}
+            <span onClick={this.props.clickClick(location)}>
+              {this.props.children}
+            </span>
+          </li>
+        )}
+      </ul>
+    )
+}
 
 class LocationListing extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      storredLocations: []
+      locations: []
     }
   }
 
-  storeLocation = (location) => e => this.setState({storredLocations: [location, ...this.state.storredLocations]})
+  storeLocation = location => e => this.setState({locations: [location, ...this.state.locations]})
 
-  removeLocation = (location) => e => this.setState({storredLocations: this.state.storredLocations.filter(_ => _.woeid != location.woeid)})
+  removeLocation = location => e => this.setState({locations: this.state.locations.filter(_ => _.woeid !== location.woeid)})
 
   render = () => (
       <div className="location-listing">
         <div className="left-collumn">
-          <LocationList items={this.props.fetchedLocations} clickClick={this.storeLocation}>
-            <LocationListActionKeep />
-          </LocationList>
+          <LocationRow items={this.props.fetchedLocations} clickClick={this.storeLocation}>
+            <span>+</span>
+          </LocationRow>
         </div>
         <div className="right-collumn">
-          <LocationList items={this.state.storredLocations} clickClick={this.removeLocation}>
-            <LocationListActionForger />
-          </LocationList>
+          <LocationRow items={this.state.locations} clickClick={this.removeLocation}>
+            <span>x</span>
+          </LocationRow>
         </div>
       </div>
     )
