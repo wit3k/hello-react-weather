47c3220884d096bf096e2572933596245ac6f2fa
Witold Adamus <witold.adamus@xsolve.pl>
2017-06-05 21:40:14 +0200

    Lon lat picker

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import LocationPicker from './components/LocationPicker'
 import LocationListing from './components/LocationListing'
 import LocationWeatherForecast from './components/LocationWeatherForecast'
 
 class App extends Component {
+  pickPosition = (pickedPosition) => (e) => this.setState({pickedPosition})
+
+  // fetchLocationForPosition = (position) => fetch(metaWeatherDomain+)
+
   render = () => (
       <div className="App">
-          <LocationPicker />
+          <LocationPicker onDataFetch={this.pickPosition} />
           <LocationListing />
           <LocationWeatherForecast daysOfWeather={[]} />
       </div>
--- src/components/LocationPicker.jsx
+++ src/components/LocationPicker.jsx
@@ -1,11 +1,48 @@
 import React, { Component } from 'react';
+import ReactCursorPosition from 'react-cursor-position';
 
 class LocationPicker extends Component {
+  trackPosition = e => this.setState({
+    longtitude: this.calculateLongtitude(e),
+    latitude: this.calculateLatitude(e)
+  })
+
+  calculateLongtitude = curPos =>
+    180 * ((curPos.position.x - ((curPos.elementDimensions ? curPos.elementDimensions.width : 0) / 2)) / ((curPos.elementDimensions ? curPos.elementDimensions.width : 0) / 2))
+
+  calculateLatitude = curPos =>
+  -180 * ((curPos.position.y - ((curPos.elementDimensions ? curPos.elementDimensions.height : 0) / 2)) / ((curPos.elementDimensions ? curPos.elementDimensions.height : 0) / 2))
+
   render = () => (
-      <div>
-          location picker
-      </div>
+    <div>
+      <ReactCursorPosition onPositionChanged={this.trackPosition}>
+        <WorldMap onDataFetch={this.props.onDataFetch(this.state)} longtitude={this.calculateLongtitude} latitude={this.calculateLatitude} />
+      </ReactCursorPosition>
+    </div>
     )
+}
+
+class WorldMap extends Component {
+  badgeStyle = () => ({
+    display: this.props.isPositionOutside ? 'hidden' : 'block',
+    position:'absolute',
+    left: this.props.position.x+'px',
+    top: this.props.position.y+'px',
+    backgroundColor: 'white',
+    borderWidth: 2+'px',
+    borderColor: 'silver',
+    borderStyle: 'solid',
+    padding: 15+'px'
+  })
+
+  render = () => (
+    <div>
+      <img alt="WorldMap" src="world-map-large.png" />
+      <div style={this.badgeStyle()} onClick={this.props.onDataFetch}>
+        Pozycja {this.props.longtitude(this.props)} : {this.props.latitude(this.props)}
+      </div>
+    </div>
+  )
 }
 
 export default LocationPicker;
